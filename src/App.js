421042367a8fdbafb52afa26c164bb9bf29ed367
long142 <hailongnguyen1424@gmail.com>
2023-12-06 11:39:06 +0700

    todoList coban1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Color  from "./components/color";
+import Color from "./components/color";
+import TodoList from "./features/Todo/components/TodoList";
+import TodoFeature from "./features/Todo/pages";
 
 function App() {
   return (
     <>
-      <Color />
+      {/* <Color /> */}
+      <TodoFeature />
     </>
   );
 }
--- src/components/color.jsx
+++ src/components/color.jsx
@@ -1,32 +1,41 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 import "./ColorBox.scss";
 
+Color.propTypes = {};
+
 function getRandomColor() {
-  const aray = ["deeppink", "green", "yellow", "black", "blue"];
-  const index = Math.trunc(Math.random() * 5);
-  return aray[index];
+  const color_list = ["black", "blue", "red", "orange"];
+  const randomIndex = Math.trunc(Math.random() * 4);
+  return color_list[randomIndex];
 }
 
-export default function Color() {
+function Color() {
   const [color, setColor] = useState(() => {
-    const initColor = localStorage.getItem("box_color") || "deeppink";
+    const initColor = localStorage.getItem("box_color") || "red";
     return initColor;
   });
-  const handleChangeColor = () => {
+
+  function handleChangeColor() {
     const newColor = getRandomColor();
     setColor(newColor);
+
     localStorage.setItem("box_color", newColor);
-  };
+  }
   return (
-    <>
-      <div>Đổi màu ô b1</div>
-      <div
-        className="color-box"
-        style={{ backgroundColor: color }}
-        onClick={handleChangeColor}
-      >
-        change color
-      </div>
-    </>
+    <div
+      className="color-box"
+      style={{
+        backgroundColor: color,
+        width: "100px",
+        height: "100px",
+        textAlign: "center",
+      }}
+      onClick={handleChangeColor}
+    >
+      color box
+    </div>
   );
 }
+
+export default Color;
