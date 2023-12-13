b24948a87d95c435b162b203e676b8db3c87c063
long142 <hailongnguyen1424@gmail.com>
2023-12-13 16:18:02 +0700

    activetodo

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Color from "./components/color";
 import TodoList from "./features/Todo/components/TodoList";
 import TodoFeature from "./features/Todo/pages";
 import AlbumFeature from "./features/AlBum/pages";
+import ColorBox from "./components/ColorBox";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
       {/* <Color /> */}
-      {/* <TodoFeature /> */}
-      <AlbumFeature />
+      <TodoFeature />
+      {/* <AlbumFeature /> */}
+      {/* <ColorBox />
+      <Counter /> */}
     </>
   );
 }
--- src/features/Todo/pages/index.jsx
+++ src/features/Todo/pages/index.jsx
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TodoList from "../components/TodoList";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  const todoList = [
+  const initTodoList = [
     {
       id: 1,
       title: "Eat",
+      status: "new",
     },
     {
       id: 2,
       title: "Sleep",
+      status: "completed",
     },
     {
       id: 3,
       title: "Code",
+      status: "new",
     },
   ];
+  const [todoList, setTodoList] = useState(initTodoList);
+
+  // cập nhật state vị trí
+  const handleTodoClick = (todo, idx) => {
+    //clone current array to the new one
+    const newTodoList = [...todoList];
+
+    console.log(todo, idx);
+    //toggle state
+    newTodoList[idx] = {
+      ...newTodoList[idx],
+      status: newTodoList[idx].status === "new" ? "completed" : "new",
+    };
+
+    //update todo list
+    setTodoList(newTodoList);
+  };
+
   return (
     <div>
       <h3>Todo list</h3>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
