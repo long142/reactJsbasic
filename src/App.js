60842a05795c9917c2811ed4d3dc9d0e3882a889
long142 <hailongnguyen1424@gmail.com>
2023-12-29 22:38:33 +0700

    sidebar menu route1

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import "./App.css";
+import "../src/styles/main.scss";
 import TodoFeature from "./features/Todo/pages";
 import AlbumFeature from "./features/AlBum/pages";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import News from "./pages/News";
+import Performances from "./pages/Performances";
+import Transactions from "./pages/Transactions";
+import Settings from "./pages/Setting";
+import Support from "./pages/Support";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/todos ">Todos</Link>
-          </li>
-          <li>
-            <Link to="/albums">Albums</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/todos" element={<TodoFeature />} />
-        <Route path="/albums" element={<AlbumFeature />} />
-      </Routes>
-    </>
+    <router>
+      <div className="App">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/performance" element={<Performances />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/transactions" element={<Transactions />} />
+        </Routes>
+      </div>
+    </router>
   );
 }
 
---
+++ src/pages/Performances.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Performances = () => {
+  return (
+    <div>
+      <h1>Performances.</h1>
+    </div>
+  );
+};
+
+export default Performances;
